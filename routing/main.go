3ccaf1db95962761557b2d3dca8cb83df67d5706
Tanvir Ahmed Sadi <tanvir.sadi10@northsouth.edu>
2024-03-09 17:34:18 +0600

    Routing

--- routing/main.go
+++ routing/main.go
@@ -9,7 +9,12 @@ import (
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/book/{title}/page/{page}", MuxRouting)
+	bookRouter := r.PathPrefix("/books").Subrouter()
+	bookRouter.HandleFunc("/{title}/page/{page}", MuxRouting)
+	bookRouter.HandleFunc("/{title}", ReadBook).Methods("GET")
+	bookRouter.HandleFunc("/{title}", CreateBook).Methods("POST")
+	bookRouter.HandleFunc("/{title}", UpdateBook).Methods("PUT")
+	bookRouter.HandleFunc("/{title}", DeleteBook).Methods("DELETE")
 	http.ListenAndServe(":8080", r)
 }
 
@@ -18,5 +23,23 @@ func MuxRouting(w http.ResponseWriter, r *http.Request) {
 	title := vars["title"]
 	page := vars["page"]
 
-	fmt.Fprintf(w, "Title: %s\n Page: %s", title, page)
+	fmt.Fprintf(w, "Title: %s\nPage: %s", title, page)
+}
+
+func ReadBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	title := vars["title"]
+	fmt.Fprintf(w, "Title: %s\n", title)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+
 }
